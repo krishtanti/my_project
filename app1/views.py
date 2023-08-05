befb00ead32bb2935090810b5335ef9d628bd0d4
krishtanti <tantikrish94@gmail.com>
2023-08-05 16:47:06 +0530

    first change

--- app1/views.py
+++ app1/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def members(request):
-    return HttpResponse("Hello world!")
+    return HttpResponse("Hello ginew world!")
